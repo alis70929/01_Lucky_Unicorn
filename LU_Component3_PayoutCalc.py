3ccbf16d3b937fdc89c32300ed8ddd5158a5702f
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-02-13 11:32:28 +1300

    PayoutCalculator started

---
+++ LU_Component3_PayoutCalc.py
@@ -0,0 +1,16 @@
+#Lucky Unicorn Component 3
+#Generate a random token
+
+import random
+HOW_MUCH = 100
+
+tokens =["horse","zebra","donkey","unicorn"]
+
+unicorn_count = 0
+zebra_count = 0
+donkey_count = 0
+for item in range(1,15):
+    chosen = random.choice(tokens)
+
+    if chosen == "unicorn"
+    print(chosen)
